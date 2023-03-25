953b1d16b1ca1551cc65d3419311a3f402372b8e
adityarajydv <adiraj625@gmail.com>
2023-03-26 00:25:13 +0530

    initialized and annotated variable

--- exercises/variables/variables2.rs
+++ exercises/variables/variables2.rs
@@ -1,13 +1,27 @@
 // variables2.rs
 // Execute `rustlings hint variables2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 fn main() {
-    let x;
+    //let x:i32=0; type annonation
+    let x:i32=10 ;
     if x == 10 {
         println!("x is ten!");
     } else {
         println!("x is not ten!");
     }
 }
+
+/*
+error[E0282]: type annotations needed
+ --> variables2.rs:7:9
+  |
+7 |     let x;
+  |         ^
+  |
+help: consider giving `x` an explicit type
+  |
+7 |     let x: /* Type */;
+  |          ++++++++++++
+ */
